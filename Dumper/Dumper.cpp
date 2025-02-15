d7402e91f18da558b9de6962f3c40821b406b94f
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2025-02-15 11:25:01 -0300

    feat(Dumper): add obtaining remote modules.

--- Dumper/Dumper.cpp
+++ Dumper/Dumper.cpp
@@ -2,9 +2,33 @@
 // Created by Dottik on 12/12/2024.
 //
 
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <libassert/assert.hpp>
+
 #include "Dumper.hpp"
 
-namespace Dottik {
-namespace Dumper {
+namespace Dottik::Dumper {
+    Dumper::Dumper(const std::int32_t dwProcessId, std::shared_ptr<Dottik::Dumper::RemoteReader> &reader) {
+        this->m_dwProcessId = dwProcessId;
+        this->m_reader = reader;
+    }
+
+    std::vector<ProcessImage> Dumper::GetAllRemoteProcessModules() {
+        HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, this->m_dwProcessId);
+
+                ASSERT(hSnapshot != nullptr && hSnapshot != INVALID_HANDLE_VALUE, "failed to create snapshot.");
+
+        MODULEENTRY32W entry{};
+                ASSERT(Module32FirstW(hSnapshot, &entry), "failed to obtain first module");
+
+        std::vector<ProcessImage> images{};
+        do {
+            images.emplace_back(entry.szModule, entry.szExePath, entry.th32ProcessID, entry.modBaseSize,
+                                entry.hModule, static_cast<void *>(entry.modBaseAddr));
+        } while (Module32NextW(hSnapshot, &entry));
+
+        return images;
+    }
 } // Dumper
-} // Dottik
+// Dottik
--- Dumper/Dumper.hpp
+++ Dumper/Dumper.hpp
@@ -3,9 +3,19 @@
 //
 
 #pragma once
+#include <cstdint>
+#include <memory>
+#include "Readers/RemoteReader.hpp"
+#include "ProcessImage.hpp"
 
 namespace Dottik::Dumper {
     class Dumper final {
+        std::shared_ptr<Dottik::Dumper::RemoteReader> m_reader;
+        std::int32_t m_dwProcessId;
+
     public:
+        explicit Dumper(std::int32_t dwProcessId, std::shared_ptr<Dottik::Dumper::RemoteReader> & reader);
+
+        std::vector<ProcessImage> GetAllRemoteProcessModules();
     };
 }
