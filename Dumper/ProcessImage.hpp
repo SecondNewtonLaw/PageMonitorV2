f60ee0b114e217d5a85355156b431a1437482920
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2025-02-15 11:24:30 -0300

    feat(ProcessImage): define structure.

---
+++ Dumper/ProcessImage.hpp
@@ -0,0 +1,27 @@
+//
+// Created by Dottik on 15/2/2025.
+//
+
+#pragma once
+
+#include <string>
+
+
+struct ProcessImage {
+    std::wstring wszModuleName;
+    std::wstring wszModulePath;
+    std::uint32_t dwOwnedBy;
+    std::uint32_t dwModuleSize;
+    HMODULE rhModule;
+    void *rlpModuleBase;
+
+    ProcessImage(wchar_t *wszModuleName, wchar_t *wszModulePath, std::uint32_t dwOwnerProcessId,
+                 std::uint32_t dwModuleSizeFromBaseAddress, HMODULE rhModule, void *rlpModuleBaseAddress) {
+        this->wszModuleName = wszModuleName;
+        this->wszModulePath = wszModulePath;
+        this->dwOwnedBy = dwOwnerProcessId;
+        this->dwModuleSize = dwModuleSizeFromBaseAddress;
+        this->rhModule = rhModule;
+        this->rlpModuleBase = rlpModuleBaseAddress;
+    }
+};
